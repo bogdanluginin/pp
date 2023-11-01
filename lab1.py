77c60e8d12168418a8d282c6f8f401436454a7f3
bogdanluginin <120667209+bogdanluginin@users.noreply.github.com>
2023-11-01 10:39:29 +0200

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/v1/hello-world-12')
-def hello_world():
-    return 'Hello World 12', 200
+@app.route('/api/v1/hello-world-<int:variant_number>', methods=['GET'])
+def hello_world(variant_number):
+    return jsonify({'message': f'Hello World {variant_number}'}), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
